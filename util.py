908e27c11d9e65fd64dafb183571378d21e56ffc
fffonceca <fffonseca@uc.cl>
2021-09-02 05:55:53 -0400

    eventos ahora guarda en Control estados

--- util.py
+++ util.py
@@ -1,8 +1,9 @@
 from interfaz import Interfaz
+from control import Control
 import pygame
 
 
-def obtener(cliente, interfaz: Interfaz):
+def obtener(cliente, interfaz: Interfaz, control: Control):
     vm1 = cliente.valvulas['valvula1'].get_value()
     vm2 = cliente.valvulas['valvula2'].get_value()
     voltajes = [vm1, vm2]
@@ -24,9 +25,10 @@ def obtener(cliente, interfaz: Interfaz):
     temp = [temp1, temp2, temp3, temp4]
 
     interfaz.setear_variables(alturas, temp, razones, voltajes)
+    control.setear_variables(alturas, [vm1, vm2, vr1, vr2])
 
 
-def Eventos(interfaz: Interfaz):
+def eventos(interfaz: Interfaz, control: Control):
     LEFT = 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -35,30 +37,35 @@ def Eventos(interfaz: Interfaz):
             if event.key == pygame.K_s:             # STOP
                 interfaz.modo = "STOP"
             if event.key == pygame.K_LEFT:         # FLECHA IZQUIERDA: bajamos voltaje 2
-                state1 = "BV2"
+                control.state1 = "BV2"
             if event.key == pygame.K_RIGHT:        # FLECHA DERECHA: subir voltaje 2
-                state1 = "SV2"
+                control.state1 = "SV2"
             if event.key == pygame.K_UP:           # FLECHA ARRIBA: subir voltaje 1
-                state1 = "SV1"
+                control.state1 = "SV1"
             if event.key == pygame.K_DOWN:          # FLECHA ABAJO: bajamos voltaje 1
-                state1 = "BV1"
+                control.state1 = "BV1"
             if event.key == pygame.K_m:             # TECLA M: MANUAL
                 interfaz.modo = "M"
             if event.key == pygame.K_a:             # TECLA A: AUTOMÁTICO
                 interfaz.modo = "A"
             if event.key == pygame.K_m:             # TECLA G: guardar datos
-                state1 = "G"
+                control.state1 = "G"
             if event.key == pygame.K_i:             # TECLA i: subir razon de flujo 1
-                state1 = "SF1"
+                control.state1 = "SF1"
             if event.key == pygame.K_k:             # TECLA k: bajar razon de flujo 1
-                state1 = "BF1"
+                control.state1 = "BF1"
             if event.key == pygame.K_l:             # TECLA l: subir razon de flujo 2
-                state1 = "SF2"
+                control.state1 = "SF2"
             if event.key == pygame.K_j:             # TECLA j: bajar razon de flujo 2
-                state1 = "BF2"
+                control.state1 = "BF2"
         if event.type == pygame.MOUSEBUTTONDOWN:    # MOUSE BTN IZQ CLICK
             if event.button == LEFT:
-                state1 = "R"
-                mouse_pos = pygame.mouse.get_pos()
-
+                control.state1 = "R"
+                control.mouse_pos = pygame.mouse.get_pos()
+                interfaz.mouse_pos = control.mouse_pos
     return True
+
+
+def funcion_handler(node, val):
+    key = node.get_parent().get_display_name().Text
+    print('key: {} | val: {}'.format(key, val))
