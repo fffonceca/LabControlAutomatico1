efcd7fad168844676fbafc6b13361c95a0e7d685
fffonceca <fffonseca@uc.cl>
2021-09-06 16:52:04 -0300

    control e interfaz partes de globals

--- globals.py
+++ globals.py
@@ -1,3 +1,9 @@
+from interfaz import Interfaz
+from control import Control
+
+
 def initialize():
-    global info_evento
+    global info_evento, interfaz, control
     info_evento = (False, 0)
+    interfaz = Interfaz()
+    control = Control()
