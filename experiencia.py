ea64e02f3e0e2c1727e248002da76cbfec45f90e
fffonceca <fffonseca@uc.cl>
2021-09-06 16:38:40 -0300

    añadiendo eventos como variable global bien def

--- experiencia.py
+++ experiencia.py
@@ -1,40 +1,24 @@
 from Libreria.cliente import Cliente
+from handler import SubHandler as SubHandlerExp
 from pygame.locals import *
-from util import obtener, eventos, funcion_handler
+from util import obtener, eventos
 from interfaz import Interfaz
 from control import Control
-import threading
-
-
-info_evento = (False, 0)
-
-
-class SubHandler(object):
-    """
-    Subscription Handler. To receive events from server for a subscription
-    data_change and event methods are called directly from receiving thread.
-    Do not do expensive, slow or network operation there. Create another
-    thread if you need to do such a thing
-    """
-    def datachange_notification(self, node, val, data):
-        thread_handler = threading.Thread(target=funcion_handler, args=(node, val))
-        thread_handler.start()
-
-    # Eventos!!
-    def event_notification(self, event):
-        global info_evento
-        info_evento = (True, event)
+import globals
 
 
 if __name__ == '__main__':
+    globals.initialize()
     cliente = Cliente("opc.tcp://localhost:4840/freeopcua/server/", suscribir_eventos=True,
-                      SubHandler=SubHandler)
+                      SubHandler=SubHandlerExp)
     cliente.conectar()
+    cliente.subscribir_cv()
+    cliente.subscribir_mv()
     interfaz = Interfaz()
     control = Control()
     while True:
         obtener(cliente, interfaz, control)
         eventos(interfaz, control)
         control.actualizar(cliente, interfaz)
-        interfaz.actualizar(info_evento)
-        info_evento = (False, 0)
+        interfaz.actualizar(globals.info_evento)
+        globals.info_evento = (False, 0)
---
+++ globals.py
@@ -0,0 +1,3 @@
+def initialize():
+    global info_evento
+    info_evento = (False, 0)
