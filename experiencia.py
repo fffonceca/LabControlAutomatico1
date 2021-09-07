e4cd608a4dae9d28117fbdc9bdc2d748f3f8fb81
fffonceca <fffonseca@uc.cl>
2021-09-06 22:27:07 -0300

    ahora control puede configurarse para el tiempo

--- experiencia.py
+++ experiencia.py
@@ -1,18 +1,24 @@
-import threading
 from Libreria.cliente import Cliente
 from handler import SubHandler as SubHandlerExp
-from pygame.locals import *
 from util import obtener, eventos
 import globals
+import time
+import threading
 
 
-def thread_programa(cliente):
+def thread_programa():
     globals.interfaz.init_interfaz()
     while True:
         eventos(globals.interfaz, globals.control)
-        globals.control.actualizar(cliente, globals.interfaz)
         globals.interfaz.actualizar(globals.info_evento)
         globals.info_evento = (False, 0)
+        time.sleep(1)
+
+
+def thread_control(cliente):
+    while True:
+        globals.control.actualizar(cliente, globals.interfaz)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
@@ -20,8 +26,10 @@ if __name__ == '__main__':
     cliente = Cliente("opc.tcp://localhost:4840/freeopcua/server/", suscribir_eventos=True,
                       SubHandler=SubHandlerExp)
     cliente.conectar()
-    hilo = threading.Thread(target=thread_programa, args=(cliente,))
+    hilo = threading.Thread(target=thread_programa)
+    hilo_control = threading.Thread(target=thread_control, args=(cliente,))
     hilo.start()
+    hilo_control.start()
     obtener(cliente, globals.interfaz, globals.control)
     cliente.subscribir_cv()
     cliente.subscribir_mv()
