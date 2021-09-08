25cca1c3a99c7db5ed107fbee6f33aac5511e0c5
fffonceca <fffonseca@uc.cl>
2021-09-08 04:32:53 -0300

    PID INICIAL incluido en parametros

--- parametros.py
+++ parametros.py
@@ -12,4 +12,8 @@ PERIODO_INTERFAZ = 0.1
 PERIODO_GRAFICOS = 1
 
 # RAM inicial
-CANTIDAD_RAM = 100
+CANTIDAD_RAM = 1000
+
+# Parametros iniciales de controlador
+#             [[Kp1, Ki1, Kd1], [Kp2, Ki2, Kd2],  [Kp3, Ki3, Kd3], [Kp4, Ki4, Kd4]]
+PID_INICIAL = [[0,   0,   0],   [0.1, 0.01, 0.02], [0.1, 0.01, 0.02], [0, 0, 0]]
